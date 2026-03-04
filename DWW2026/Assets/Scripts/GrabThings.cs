42cb5c5cb5d39f7d89b0f7a1fec70bae50d63a2c
Dorian Muhametaj <muhameta@shernet.sheridancollege.ca>
2026-03-03 20:36:37 -0500

    Itch build with collision and slight feedback, made for testing perspective of arms and game feel

--- DWW2026/Assets/Scripts/GrabThings.cs
+++ DWW2026/Assets/Scripts/GrabThings.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class GrabThings : MonoBehaviour
 {
     private StarterAssetsInputs _input;
+    public Transform monkeyBody;
+    private bool repeat;
+    private Vector3 raise;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,21 +16,25 @@ public class GrabThings : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (repeat)
+        {
+            raise.y = 2;
+            monkeyBody.position += raise * Time.deltaTime;
+        }
     }
 
     private void OnTriggerStay(Collider collision)
     {
         if (collision.gameObject.layer == 6)
         {
-            if (_input.lGrab)
+            if (_input.lGrab && repeat == false)
             {
-                Debug.Log("Grab");
+                repeat = true; 
             }
 
-            else if (_input.lRelease)
+            else if (!_input.lGrab)
             {
-                Debug.Log("Let Go");
+                repeat = false; 
             }
         }
       
--- DWW2026/Assets/Scripts/GrabThingsRight.cs
+++ DWW2026/Assets/Scripts/GrabThingsRight.cs
@@ -1,33 +1,44 @@
 using StarterAssets;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class GrabThingsRight : MonoBehaviour
 {
     private StarterAssetsInputs _input;
+    public Transform monkeyBody; 
+    private bool repeat;
+    private Vector3 raise; 
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _input = GetComponentInParent<StarterAssetsInputs>();
+        repeat = false;
+        raise = monkeyBody.position; 
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (repeat)
+        {
+            raise.y = 2;
+            monkeyBody.position += raise * Time.deltaTime; 
+        }
     }
 
     private void OnTriggerStay(Collider collision)
     {
         if (collision.gameObject.layer == 6)
         {
-            if (_input.rGrab)
+            if (_input.rGrab && repeat == false)
             {
-               // Debug.Log("Grab");
+                repeat = true; 
             }
 
-            if (_input.rRelease)
+            else if (!_input.rGrab)
             {
-                Debug.Log("Let Go");
+                repeat = false; 
             }
         }
 
