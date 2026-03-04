ef2c40ddf14ca73e352d6454e39684d2a48b2fde
doodlecultistREAL <juliajulianleyva@gmail.com>
2026-03-03 19:24:07 -0500

    monkey fixed.

---
+++ DWW2026/Assets/Scripts/ArmMoveNew.cs
@@ -0,0 +1,61 @@
+using StarterAssets;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ArmMoveNew : MonoBehaviour
+{
+    public float rotationSpeed;
+    public GameObject leftHand; 
+    public GameObject rightHand;
+
+    Vector3 rotBaseR;
+    Vector3 rotBaseL;
+
+    Vector3 lHandRot;
+    Vector3 rHandRot;
+
+    float rangeMotion = 90;
+
+    private StarterAssetsInputs _input;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _input = GetComponentInParent<StarterAssetsInputs>();
+
+        rotBaseL = leftHand.transform.localEulerAngles;
+        rotBaseR = rightHand.transform.localEulerAngles;
+        lHandRot = leftHand.transform.localEulerAngles;
+        rHandRot = rightHand.transform.localEulerAngles;
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_input.lArm.y != 0)
+        {
+            lHandRot.x = rotBaseL.y + (rangeMotion * _input.lArm.y);
+        }
+
+        if(_input.lArm.x != 0)
+        {
+            lHandRot.z = rotBaseL.z + (rangeMotion * _input.lArm.x); 
+        }
+
+        if (_input.rArm.y != 0)
+        {
+            rHandRot.x = rotBaseR.y + (rangeMotion * -_input.rArm.y);
+        }
+
+        if (_input.rArm.x != 0)
+        {
+            rHandRot.z = rotBaseR.z + (rangeMotion * -_input.rArm.x);
+        }
+
+
+        leftHand.transform.localEulerAngles = lHandRot;
+        rightHand.transform.localEulerAngles = rHandRot;
+    }
+}
---
+++ DWW2026/Assets/Scripts/HandMove.cs
@@ -0,0 +1,108 @@
+using Cinemachine.Utility;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class HandMove : MonoBehaviour
+{
+
+    //where the hand is trying to get to
+    public GameObject endPoint;
+
+    //the hands in question
+    public GameObject hand;
+
+    //hand rigid body to apply forces to
+    public Rigidbody handBody;
+
+    Vector3 handPos;
+    Vector3 endPos;
+    Vector3 endForce;
+
+    //force modifiers
+
+    float forcePower = 10;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //setting values
+        handPos = hand.transform.position;
+        endPos = endPoint.transform.position;
+
+           
+
+        //adding force!
+        handBody.AddForce(endForce.x, endForce.y, endForce.z);
+
+
+        //multiplying force by the modifier
+
+       
+
+        //if increase the amount of force the farther away the hand is from the end point.
+
+        endForce = Vector3.one * forcePower;
+
+
+
+        //X AXIS
+        if (handPos.x >= endPos.x)
+        {
+            endForce.x = -1;
+
+
+        }
+        else if (handPos.x <= endPos.x)
+        {
+
+            endForce.x = 1;
+
+
+        }
+
+
+        //Y AXIS
+
+        if (handPos.y >= endPos.y)
+        {
+            endForce.y = -1;
+
+
+        }
+        else if (handPos.y <= endPos.y)
+        {
+
+            endForce.y = 1;
+
+
+        }
+
+        //Z AXIS
+
+        if (handPos.z >= endPos.z)
+        {
+            endForce.z = -1;
+
+
+        }
+        else if (handPos.z <= endPos.z)
+        {
+
+            endForce.z = 1;
+
+
+        }
+
+
+
+
+    }
+}
