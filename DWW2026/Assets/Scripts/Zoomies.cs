221b95c397d3367e6f6c25ef1e529c26a7ccca5d
LauraSemino <150286546+LauraSemino@users.noreply.github.com>
2026-03-05 14:55:23 -0500

    wooshing effect added

--- DWW2026/Assets/Scripts/Zoomies.cs
+++ DWW2026/Assets/Scripts/Zoomies.cs
@@ -17,14 +17,45 @@ public class Zoomies : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {   
+    {  
+
         velocity = GetComponent<Rigidbody>().linearVelocity.magnitude;
-        timer += velocity * Time.deltaTime;
+        if (velocity > 6) 
+        {
+            timer += velocity * Time.deltaTime * 60;
+        }
+        else
+        {
+            wooshies[0].SetActive(false);
+            wooshies[1].SetActive(false);
+            wooshies[2].SetActive(false);
+            timer = 0;
+        }
+
         if (timer > 0 && timer <= 33f)
         {
             wooshies[0].SetActive(true);
             wooshies[1].SetActive(false);
             wooshies[2].SetActive(false);
+            wooshies[0].GetComponent<RawImage>().color = new Color(255, 255, 255, velocity/30);
+        }
+        if (timer > 33 && timer <= 66f)
+        {
+            wooshies[0].SetActive(false);
+            wooshies[1].SetActive(true);
+            wooshies[2].SetActive(false);
+            wooshies[1].GetComponent<RawImage>().color = new Color(255, 255, 255, velocity/30);
+        }
+        if (timer > 66 && timer <= 99f)
+        {
+            wooshies[0].SetActive(false);
+            wooshies[1].SetActive(false);
+            wooshies[2].SetActive(true);
+            wooshies[2].GetComponent<RawImage>().color = new Color(255, 255, 255, velocity/30);
+        }
+        if (timer >= 100)
+        {
+            timer = 0;
         }
 
     }
